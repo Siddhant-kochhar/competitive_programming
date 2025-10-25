001e160023ef7ef36a0fed373a857742fccd2d83
Siddhant Kochhar <109429625+Siddhant-kochhar@users.noreply.github.com>
2025-10-25 12:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.64%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -0,0 +1,14 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        initial = 1  
+        curr = 1     
+        res = 0       
+
+        for i in range(1, n + 1):
+            if i % 7 == 1 and i != 1: 
+                initial += 1           
+                curr = initial         
+            res += curr                
+            curr += 1                  
+
+        return (res)
